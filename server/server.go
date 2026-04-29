cb10ceb51d668480d95ff2af45ff4919e90b40bd
Adam Tauber <asciimoo@gmail.com>
2026-04-29 17:01:46 +0200

    [fix] allow /api/delete calls from the extension

--- server/server.go
+++ server/server.go
@@ -371,7 +371,7 @@ func withCSRF(handler endpointHandler) endpointHandler {
 			return
 		}
 		// Allow add, config requests from the addons
-		for _, p := range []string{"/add", "/api/add", "/api/config", "/api/rules"} {
+		for _, p := range []string{"/add", "/api/add", "/api/config", "/api/rules", "/api/delete"} {
 			if c.Request.URL.Path != c.Config.BasePathPrefix()+p {
 				continue
 			}
